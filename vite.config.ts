76e6cfbe61fd3508619153ac2fc116e6d21769ae
Liam <liamkinazo12@gmail.com>
2026-02-26 22:42:30 +0800

    Add VueDevTools plugin to Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import path from 'node:path'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
+import VueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue(), tailwindcss(), VueDevTools()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
