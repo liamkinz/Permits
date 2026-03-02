98e0279cb46bada3b6f02faaedfca81eb69ffdb2
Liam <liamkinazo12@gmail.com>
2026-03-02 22:31:27 +0800

    Refactor layout and add new components: FooterSection, ModeToggle, and OuterNavbar; update routing and styles

---
+++ src/components/FooterSection.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const currentYear = new Date().getFullYear()
+import TypographyMuted from '@/components/ui/typography/TypographyMuted.vue'
+</script>
+
+<template>
+  <footer class="w-full border-t bg-background py-6 md:py-0">
+    <div
+      class="container mx-auto flex flex-col items-center justify-center gap-4 px-4 md:h-24 md:flex-row md:px-8"
+    >
+      <div class="flex flex-col items-center gap-4 md:flex-row md:gap-2">
+        <TypographyMuted>
+          Built by
+          <a
+            href="#"
+            target="_blank"
+            rel="noreferrer"
+            class="font-medium underline underline-offset-4"
+          >
+            Permits
+          </a>
+          . &copy; {{ currentYear }} All rights reserved.
+        </TypographyMuted>
+      </div>
+    </div>
+  </footer>
+</template>
--- src/router/router.ts
+++ src/router/router.ts
@@ -1,21 +1,128 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
+// import { useAuthStore } from "@/stores/authStore";
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'Landing',
+    component: () => import('@/pages/LandingPage/LandingView.vue'),
+  },
+  // {
+  //   path: "/home",
+  //   name: "home",
+  //   component: () => import("@/pages/HomePage/HomeView.vue"),
+  //   meta: {
+  //     title: "Home",
+  //     guard: "auth",
+  //   },
+  // },
+  // {
+  //   path: "/admin",
+  //   name: "admin",
+  //   component: () => import("@/pages/AdminPage/AdminView.vue"),
+  //   meta: {
+  //     title: "Admin Dashboard",
+  //     /*  guard: 'auth' */
+  //   },
+  //   redirect: "/admin/dashboard",
+  //   children: [
+  //     {
+  //       path: "dashboard",
+  //       name: "admin-dashboard",
+  //       component: () =>
+  //         import("@/pages/AdminPage/components/AdminDashboard.vue"),
+  //       meta: {
+  //         title: "Admin Dashboard",
+  //         /*  guard: 'auth' */
+  //       },
+  //     },
+  //     {
+  //       path: "billing",
+  //       name: "admin-billing",
+  //       component: () =>
+  //         import("@/pages/AdminPage/components/AdminBilling.vue"),
+  //       meta: {
+  //         title: "Admin Billing",
+  //         /*  guard: 'auth' */
+  //       },
+  //     },
+  //   ],
+  // },
+  // {
+  //   path: "/signin",
+  //   name: "login",
+  //   component: () => import("@/pages/auth/LoginView.vue"),
+  //   meta: {
+  //     title: "Login",
+  //     guard: "guest",
+  //   },
+  // },
+  // // Registration
+  // {
+  //   path: "/signup",
+  //   name: "register",
+  //   component: () => import("@/pages/auth/RegisterView.vue"),
+  //   meta: {
+  //     title: "User Registration",
+  //     guard: "guest",
+  //   },
+  // },
+  // // Error Pages
+  // {
+  //   path: "/access-denied",
+  //   name: "access-denied",
+  //   component: () => import("@/pages/errors/AccessDenied.vue"),
+  //   meta: {
+  //     title: "Access Denied",
+  //   },
+  // },
+  // {
+  //   path: "/business-owner",
+  //   name: "business-owner",
+  //   component: () => import("@/pages/BusinessOwnerPage/BusinessOwnerView.vue"),
+  //   meta: {
+  //     title: "Business Owner Dashboard",
+  //     /*  guard: 'auth' */
+  //   },
+  // },
+  // {
+  //   path: "/business-owner/pick-location",
+  //   name: "gmap-location-picker",
+  //   component: () =>
+  //     import("@/pages/BusinessOwnerPage/GmapPickLocationView.vue"),
+  //   meta: {
+  //     title: "Pick Your Location",
+  //     /*   guard: 'auth' */
+  //   },
+  // },
+  // {
+  //   path: "/:pathMatch(.*)*",
+  //   redirect: "/page-not-found",
+  // },
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // Lazy-loaded route
-      component: () => import('@/views/AboutView.vue')
-    }
-  ]
+  history: createWebHistory(),
+  routes,
 })
+
+// // Navigation guards
+// router.beforeEach((to, _from, next) => {
+//   const authStore = useAuthStore();
+//   const isAuthenticated = authStore.isAuthenticated;
+
+//   // Check if the route requires authentication
+//   if (to.meta.guard === "auth" && !isAuthenticated) {
+//     // Redirect to login if not authenticated
+//     next({ name: "login" });
+//   }
+//   // Check if the route is for guests only (like login/register)
+//   else if (to.meta.guard === "guest" && isAuthenticated) {
+//     // Redirect to home if already authenticated
+//     next({ name: "home" });
+//   } else {
+//     next();
+//   }
+// });
 
 export default router
